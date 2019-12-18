077d39aba65f5ec5fa89b496fb7224ec9fe66cbc
wanghr74 <wanghr74@qq.com>
2019-12-18 14:42:12 +0800

    201912181442

---
+++ src/com/company/teacher.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class teacher {
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    int age;
+    String name;
+
+
+}
