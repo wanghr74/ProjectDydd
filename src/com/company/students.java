9223edceac880bad751df56e9c74e06ee9c23159
wanghr74 <wanghr74@qq.com>
2019-12-18 13:55:38 +0800

    20191218

---
+++ src/com/company/students.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class students {
+    public int getSn() {
+        return sn;
+    }
+
+    public void setSn(int sn) {
+        this.sn = sn;
+    }
+
+    public int sn;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String name;
+
+
+}
